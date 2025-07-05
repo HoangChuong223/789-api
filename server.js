4d65c4974d4a12b47e97a1428a93f1d3f64aeaa3
HoangChuong223 <umff902@gmail.com>
2025-07-06 01:26:05 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8888;
 app.use(cors());
 
 let currentData = {
